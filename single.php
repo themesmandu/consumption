7b728ba36c91a8b0472e30c9dfab918d5f07bea4
rupeshchamling <rupeshrakhamicha@gmail.com>
2019-06-20 14:56:57 +0545

    added place for google ad in silgle page

--- single.php
+++ single.php
@@ -27,87 +27,91 @@ get_header();
 				$current_categorys = get_the_category();
 				$previous_post     = get_previous_post();
 				?>
-				<div class="video-navigation blog-content">
-					<?php if ($previous_post) : ?>
-						<div class="previous">
-							<span>Previous Video</span>
-							<a href="<?php echo get_permalink($previous_post->ID); ?>">
-								<div class="ytcont">
-									<?php echo apply_filters('the_content', $previous_post->post_content); ?>
-									<h2 class="entry-title"><?php echo $previous_post->post_title; ?></h2>
-								</div>
-							</a>
-						</div>
-					<?php endif; ?>
-					<?php
-					$next_post = get_next_post();
-					if ($next_post) :
-						?>
-						<div class="next">
-							<span>Next Video</span>
-							<a href="<?php echo get_permalink($next_post->ID); ?>">
-								<div class="ytcont">
-									<?php echo apply_filters('the_content', $next_post->post_content); ?>
-									<h2 class="entry-title"><?php echo $next_post->post_title; ?></h2>
-								</div>
-							</a>
-						</div>
-					<?php endif; ?>
-				</div>
-				<div class="video-navigation blog-content random">
-					<?php
-					$previous_random = new WP_Query(
-						array(
-							'posts_per_page' => 1,
-							'orderby'        => 'rand',
-							'cat'            => $current_categorys[0]->term_id,
-							'post__not_in'   => array(get_the_ID()),
-						)
-					);
-					if ($previous_random->have_posts()) :
-						while ($previous_random->have_posts()) :
-							$previous_random->the_post();
-							$previous_random_id = $previous_random->ID;
-							?>
-							<div class="previous-randon previous">
-
-								<a href="<?php echo get_permalink($previous_random->ID); ?>">
+				<div class="navigator_wrapper">
+					<div class="video-navigation blog-content">
+						<?php if ($previous_post) : ?>
+							<div class="previous">
+								<a href="<?php echo get_permalink($previous_post->ID); ?>">
 									<div class="ytcont">
-										<?php the_content(''); ?>
-										<h2 class="entry-title"><?php the_title(); ?></h2>
+										<?php echo apply_filters('the_content', $previous_post->post_content); ?>
+										<h2 class="entry-title"><?php echo $previous_post->post_title; ?></h2>
 									</div>
 								</a>
-								<span><a href="<?php echo get_permalink($previous_random->ID); ?>">Previous Random</a></span>
-								
+								<span>Previous Video</span>
 							</div>
-						<?php endwhile; ?>
-					<?php endif; ?>
-					<?php
-					$next_random = new WP_Query(
-						array(
-							'posts_per_page' => 1,
-							'orderby'        => 'rand',
-							'cat'            => $current_categorys[0]->term_id,
-							'post__not_in'   => array(get_the_ID(), $previous_random_id),
-						)
-					);
-					if ($next_random->have_posts()) :
-						while ($next_random->have_posts()) :
-							$next_random->the_post();
+						<?php endif; ?>
+						<?php
+						$next_post = get_next_post();
+						if ($next_post) :
 							?>
-							<div class="next-randon next">
-								<a href="<?php echo get_permalink($next_random->ID); ?>">
+							<div class="next">
+								<a href="<?php echo get_permalink($next_post->ID); ?>">
 									<div class="ytcont">
-										<?php the_content(''); ?>
-										<h2 class="entry-title"><?php the_title(); ?></h2>
+										<?php echo apply_filters('the_content', $next_post->post_content); ?>
+										<h2 class="entry-title"><?php echo $next_post->post_title; ?></h2>
 									</div>
 								</a>
-
-								<span><a href="<?php echo get_permalink($next_random->ID); ?>">Next Random</a></span>
-
+								<span>Next Video</span>
 							</div>
-						<?php endwhile; ?>
-					<?php endif; ?>
+						<?php endif; ?>
+					</div>
+
+					<div class="ad">
+					</div>
+
+					<div class="video-navigation blog-content random">
+						<?php
+						$previous_random = new WP_Query(
+							array(
+								'posts_per_page' => 1,
+								'orderby'        => 'rand',
+								'cat'            => $current_categorys[0]->term_id,
+								'post__not_in'   => array(get_the_ID(),$previous_post->ID,$next_post->ID),
+							)
+						);
+						if ($previous_random->have_posts()) :
+							while ($previous_random->have_posts()) :
+								$previous_random->the_post();
+								$previous_random_id = $previous_random->ID;
+								?>
+								<div class="previous-randon previous">
+
+									<a href="<?php echo get_permalink($previous_random->ID); ?>">
+										<div class="ytcont">
+											<?php the_content(''); ?>
+										</div>
+									</a>
+									<span><a href="<?php echo get_permalink($previous_random->ID); ?>">Previous Random</a></span>
+
+								</div>
+							<?php endwhile; ?>
+						<?php endif; ?>
+						<?php
+						$next_random = new WP_Query(
+							array(
+								'posts_per_page' => 1,
+								'orderby'        => 'rand',
+								'cat'            => $current_categorys[0]->term_id,
+								'post__not_in'   => array(get_the_ID(), $previous_random_id,$next_post->ID,$previous_post->ID),
+							)
+						);
+						if ($next_random->have_posts()) :
+							while ($next_random->have_posts()) :
+								$next_random->the_post();
+								?>
+								<div class="next-randon next">
+									<a href="<?php echo get_permalink($next_random->ID); ?>">
+										<div class="ytcont">
+											<?php the_content(''); ?>
+										</div>
+									</a>
+
+									<span><a href="<?php echo get_permalink($next_random->ID); ?>">Next Random</a></span>
+
+								</div>
+							<?php endwhile; ?>
+						<?php endif; ?>
+					</div>
 				</div>
 				<?php
 				// If comments are open or we have at least one comment, load up the comment template.
