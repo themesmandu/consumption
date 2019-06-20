ef60a96ae5a205433882b935a29776816ddf8db9
sauravxtha <sauravxtha@gmail.com>
2019-06-20 13:21:04 +0545

    Update content-custom.php

--- template-parts/content-custom.php
+++ template-parts/content-custom.php
@@ -11,27 +11,28 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<a href="<?php echo esc_url(get_permalink()); ?>">
+	<a href="<?php echo esc_url( get_permalink() ); ?>">
 		<?php
 		echo '<div class="ytcont">';
-		the_content('');
+		the_content( '' );
 		echo '</div>';
 
 		?>
 	</a>
 	<header class="entry-header">
 		<?php
-		if (is_singular()) :
-			the_title('<h1 class="entry-title">', '</h1>');
+		if ( is_singular() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
-			the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
-		endif; ?>
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;
+		?>
 	</header><!-- .entry-header -->
 
 	<?php
 	wp_link_pages(
 		array(
-			'before' => '<div class="page-links">' . esc_html__('Pages:', 'consumption'),
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'consumption' ),
 			'after'  => '</div>',
 		)
 	);
