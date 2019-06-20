a795b92be6b12599a2329e5b01a525e1b44e22de
rupeshchamling <rupeshrakhamicha@gmail.com>
2019-06-20 14:04:16 +0545

    Update single.php

--- single.php
+++ single.php
@@ -13,45 +13,45 @@ get_header();
 	<div class="row row-wrap">
 		<main id="main">
 			<section class="content-heading">
-				<h1><?php echo esc_html( get_theme_mod( 'front_heading' ) ); ?></h1>
-				<p><?php echo esc_html( get_theme_mod( 'front_description' ) ); ?></p>
-				<?php echo base64_decode( get_theme_mod( 'front_ad_area' ) ); ?>
+				<h1><?php echo esc_html(get_theme_mod('front_heading')); ?></h1>
+				<p><?php echo esc_html(get_theme_mod('front_description')); ?></p>
+				<?php echo base64_decode(get_theme_mod('front_ad_area')); ?>
 				<?php get_search_form(); ?>
 			</section>
 			<?php
-			while ( have_posts() ) :
+			while (have_posts()) :
 				the_post();
 
-				get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part('template-parts/content', get_post_type());
 
 				$current_categorys = get_the_category();
 				$previous_post     = get_previous_post();
 				?>
 				<div class="video-navigation blog-content">
-				<?php if ( $previous_post ) : ?>
-					<div class="previous">
-						<span>Previous Video</span>
-						<a href="<?php echo get_permalink( $previous_post->ID ); ?>">
-							<div class="ytcont">
-								<?php echo apply_filters( 'the_content', $previous_post->post_content ); ?>
-								<h2 class="entry-title"><?php echo $previous_post->post_title; ?></h2>
-							</div>
-						</a>
-					</div>
+					<?php if ($previous_post) : ?>
+						<div class="previous">
+							<span>Previous Video</span>
+							<a href="<?php echo get_permalink($previous_post->ID); ?>">
+								<div class="ytcont">
+									<?php echo apply_filters('the_content', $previous_post->post_content); ?>
+									<h2 class="entry-title"><?php echo $previous_post->post_title; ?></h2>
+								</div>
+							</a>
+						</div>
 					<?php endif; ?>
 					<?php
 					$next_post = get_next_post();
-					if ( $next_post ) :
+					if ($next_post) :
 						?>
-					<div class="next">
-						<span>Next Video</span>
-						<a href="<?php echo get_permalink( $next_post->ID ); ?>">
-							<div class="ytcont">
-								<?php echo apply_filters( 'the_content', $next_post->post_content ); ?>
-								<h2 class="entry-title"><?php echo $next_post->post_title; ?></h2>
-							</div>
-						</a>
-					</div>
+						<div class="next">
+							<span>Next Video</span>
+							<a href="<?php echo get_permalink($next_post->ID); ?>">
+								<div class="ytcont">
+									<?php echo apply_filters('the_content', $next_post->post_content); ?>
+									<h2 class="entry-title"><?php echo $next_post->post_title; ?></h2>
+								</div>
+							</a>
+						</div>
 					<?php endif; ?>
 				</div>
 				<div class="video-navigation blog-content random">
@@ -61,24 +61,25 @@ get_header();
 							'posts_per_page' => 1,
 							'orderby'        => 'rand',
 							'cat'            => $current_categorys[0]->term_id,
-							'post__not_in'   => array( get_the_ID() ),
+							'post__not_in'   => array(get_the_ID()),
 						)
 					);
-					if ( $previous_random->have_posts() ) :
-						while ( $previous_random->have_posts() ) :
+					if ($previous_random->have_posts()) :
+						while ($previous_random->have_posts()) :
 							$previous_random->the_post();
 							$previous_random_id = $previous_random->ID;
 							?>
-						<div class="previous-randon previous">
-						<span>Previous Random</span>
-						<a href="<?php echo get_permalink( $previous_random->ID ); ?>">
-							<div class="ytcont">
-								<?php the_content( '' ); ?>
-								<h2 class="entry-title"><?php the_title(); ?></h2>
-							</div>
-						</a>
+							<div class="previous-randon previous">
 
-						</div>
+								<a href="<?php echo get_permalink($previous_random->ID); ?>">
+									<div class="ytcont">
+										<?php the_content(''); ?>
+										<h2 class="entry-title"><?php the_title(); ?></h2>
+									</div>
+								</a>
+								<span><a href="<?php echo get_permalink($previous_random->ID); ?>">Previous Random</a></span>
+								
+							</div>
 						<?php endwhile; ?>
 					<?php endif; ?>
 					<?php
@@ -87,29 +88,30 @@ get_header();
 							'posts_per_page' => 1,
 							'orderby'        => 'rand',
 							'cat'            => $current_categorys[0]->term_id,
-							'post__not_in'   => array( get_the_ID(), $previous_random_id ),
+							'post__not_in'   => array(get_the_ID(), $previous_random_id),
 						)
 					);
-					if ( $next_random->have_posts() ) :
-						while ( $next_random->have_posts() ) :
+					if ($next_random->have_posts()) :
+						while ($next_random->have_posts()) :
 							$next_random->the_post();
 							?>
-						<div class="next-randon next">
-						<span>Next Random</span>
-						<a href="<?php echo get_permalink( $next_random->ID ); ?>">
-							<div class="ytcont">
-								<?php the_content( '' ); ?>
-								<h2 class="entry-title"><?php the_title(); ?></h2>
-							</div>
-						</a>
+							<div class="next-randon next">
+								<a href="<?php echo get_permalink($next_random->ID); ?>">
+									<div class="ytcont">
+										<?php the_content(''); ?>
+										<h2 class="entry-title"><?php the_title(); ?></h2>
+									</div>
+								</a>
 
-						</div>
+								<span><a href="<?php echo get_permalink($next_random->ID); ?>">Next Random</a></span>
+
+							</div>
 						<?php endwhile; ?>
 					<?php endif; ?>
 				</div>
 				<?php
 				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
+				if (comments_open() || get_comments_number()) :
 					comments_template();
 				endif;
 
